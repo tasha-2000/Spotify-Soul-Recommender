e86659c166a43f5dd642082a8c98b70744015c24
tasha-2000 <103650882+tasha-2000@users.noreply.github.com>
2024-02-21 16:16:48 +0000

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# input your credentials here
+clientId = 'your_spotify_client_id'
+clientSecret = 'your_spotify_client_secret'
+redirectUri = 'your_app_redirect_uri'
+username = 'your_spotify_username'
