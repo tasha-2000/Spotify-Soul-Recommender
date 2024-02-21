12a5e83317d1e8768f041c69b1856e73544a229d
tasha-2000 <103650882+tasha-2000@users.noreply.github.com>
2024-02-21 14:15:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import model
 from model import track_IDs
 import spotipy
 import spotipy.util as util
-import credentials2
+import secrets
 import pandas as pd
 
 scope = 'user-top-read playlist-modify-private playlist-modify-public user-library-read user-top-read'
-token = util.prompt_for_user_token(credentials2.username, scope, client_id=credentials2.client_id, client_secret= credentials2.client_secret, redirect_uri=credentials2.redirect_uri)
+token = util.prompt_for_user_token(secrets.username, scope, client_id=secrets.client_id, client_secret= secrets.client_secret, redirect_uri=secrets.redirect_uri)
 if token:
     sp = spotipy.Spotify(auth=token)
 else:
-    print("Can't get token for", credentials2.username)
+    print("Can't get token for", secrets.username)
 
 def createPlaylist(name, description):
     ids = track_IDs
-    playlist = sp.user_playlist_create(credentials2.username,name,True,False,description)
+    playlist = sp.user_playlist_create(secrets.username,name,True,False,description)
     playlist_id = playlist['id']
     with open('reccomendations.txt','r') as file:
         for line in file:
@@ -22,12 +22,12 @@ def createPlaylist(name, description):
             track_URI = sp.track(track_id)['uri']
             ids.append(track_URI)
     
-    sp.user_playlist_add_tracks(user=credentials2.username, playlist_id=playlist_id, tracks=ids, position=None)
+    sp.user_playlist_add_tracks(user=secrets.username, playlist_id=playlist_id, tracks=ids, position=None)
     print('added all songs to playlist')
 
 def main():
-    playlistName = 'New Soul'
-    playlistDescription = 'Testing Out My Reccomender'
+    playlistName = 'Spotify Soul'
+    playlistDescription = 'This playlist was specially curated using the Spotify Soul Recommender Project created by Tasha. https://github.com/tasha-2000/Spotify-Soul-Recommender'
 
     createPlaylist(playlistName,playlistDescription)
     
