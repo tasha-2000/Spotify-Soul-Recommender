ce00f11b444b1e8b2e88f9a3a56743d17c5eda48
tasha-2000 <103650882+tasha-2000@users.noreply.github.com>
2024-02-26 10:18:39 +0000

    Update initialiseDataset.py

--- initialiseDataset.py
+++ initialiseDataset.py
@@ -1,15 +1,3 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-import secrets
-
-# Function to initialize Spotify client
-def InitSpotifyClient():
-    scope = 'user-top-read playlist-modify-private playlist-modify-public user-library-read'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.clientId,
-                                                   client_secret=secrets.clientSecret,
-                                                   redirect_uri=secrets.redirectUri,
-                                                   scope=scope))
-    return sp
 
 # Function to get artist information
 def GetArtistInfo(sp, artistId):
@@ -51,26 +39,3 @@ def GetTracksFromPlaylist(sp, playlistIds):
         results = sp.playlist_items(playlistId, limit=50, fields='items.track.id')
         trackIds.extend([item['track']['id'] for item in results['items'] if item['track']])
     return trackIds
-
-# Main function to orchestrate the flow
-def Main():
-    sp = InitSpotifyClient()
-
-    # stores the userFavs track IDs into a text file ( comment out if you've already done this)
-    trackIds = FindSoulfulTracks(sp)
-    with open('userFavs.txt', 'w') as file:
-        for trackId in trackIds:
-            file.write(trackId + '\n')
-    print(trackIds)
-
-    # creates a text file with around 8000 track IDs - this is your soulful song library ( comment out if you've already done this)
-    # keywords = {'contemporary', 'r&b', 'lofi', 'soul', 'jazz', 'neo soul', 'blues', 'chillwave', 'lo-fi', 'chill', 'soul'}
-    # playlistIds = FindSoulfulPlaylists(sp, keywords)
-    # allTrackIds = GetTracksFromPlaylist(sp, playlistIds)
-    # with open('track_ids.txt', 'w') as file:
-    #     for Id in allTrackIds:
-    #         file.write(Id + '\n')
-    # print(f"Total tracks collected: {len(allTrackIds)}")
-
-if __name__ == "__main__":
-    Main()
